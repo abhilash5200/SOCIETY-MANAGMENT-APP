e21e0b625b8ccc172c63829e49e799e728cfd037
BADDIPALLY ABHILASH REDDY <abhilashreddy5200@gmail.com>
2026-05-17 16:57:53 +0530

    .env

--- society-frontend/src/socket/socket.js
+++ society-frontend/src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL =
-  "http://localhost:5000";
+  "https://society-managment-app.onrender.com";
 
 export const socket =
   io(SOCKET_URL, {
